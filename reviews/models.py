bea050a1feedeac5abaea49898f87e5fb5b3d4e9
Dhruvi Jhanjhri <dhruvijhanjhri@gmail.com>
2026-06-22 16:00:58 +0530

    Stable booking and payment system completed

---
+++ reviews/models.py
@@ -0,0 +1,61 @@
+from django.db import models
+from django.conf import settings
+
+from hotels.models import Hotel
+from packages.models import TravelPackage
+
+
+class HotelReview(models.Model):
+
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE
+    )
+
+    hotel = models.ForeignKey(
+        Hotel,
+        on_delete=models.CASCADE,
+        related_name="reviews"
+    )
+
+    rating = models.IntegerField()
+
+    review_text = models.TextField()
+
+    created_at = models.DateTimeField(
+        auto_now_add=True
+    )
+
+    def __str__(self):
+        return (
+            f"{self.user.username} - "
+            f"{self.hotel.name}"
+        )
+
+
+class PackageReview(models.Model):
+
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE
+    )
+
+    package = models.ForeignKey(
+        TravelPackage,
+        on_delete=models.CASCADE,
+        related_name="reviews"
+    )
+
+    rating = models.IntegerField()
+
+    review_text = models.TextField()
+
+    created_at = models.DateTimeField(
+        auto_now_add=True
+    )
+
+    def __str__(self):
+        return (
+            f"{self.user.username} - "
+            f"{self.package.name}"
+        )
--- tripease/settings.py
+++ tripease/settings.py
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
     'contact',
     'hotels',
     'packages',
+    'reviews',
 ]
 
 MIDDLEWARE = [
